c2893cf01e862e0c4a3a8ef3c8b1717e7294fe1f
Yeseo Kim <YKim@lcs.on.ca>
2021-10-13 10:56:13 -0400

    Initial Commit

---
+++ AgeGuess/AgeGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgeGuessApp.swift
+//  AgeGuess
+//
+//  Created by Yeseo Kim on 2021-10-13.
+//
+
+import SwiftUI
+
+@main
+struct AgeGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
