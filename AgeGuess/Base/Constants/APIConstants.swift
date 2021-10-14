740827f25fe9b1a7fa17f0846c326f5e0bc74ee0
Yeseo Kim <YKim@lcs.on.ca>
2021-10-14 09:20:45 -0400

    baseUrl added to APIConstants

---
+++ AgeGuess/Base/Constants/APIConstants.swift
@@ -0,0 +1,12 @@
+//
+//  APIConstants.swift
+//  AgeGuess
+//
+//  Created by Yeseo Kim on 2021-10-14.
+//
+
+import Foundation
+
+enum APIConstants {
+    static let baseUrl = "https://api.agify.io/"
+}
