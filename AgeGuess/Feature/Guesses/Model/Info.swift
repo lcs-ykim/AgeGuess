03af4320e5619dfcac4e295f5c191c8e48bd05f2
Yeseo Kim <YKim@lcs.on.ca>
2021-10-14 08:58:02 -0400

    built 'Info' and an array of dummy data

--- AgeGuess/Feature/Guesses/Model/Info.swift
+++ AgeGuess/Feature/Guesses/Model/Info.swift
@@ -2,19 +2,23 @@
 //  Info.swift
 //  AgeGuess
 //
-//  Created by Yeseo Kim on 2021-10-13.
+//  Created by Yeseo Kim on 2021-10-14.
 //
 
-import SwiftUI
+import Foundation
 
-struct Info: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct Info: Decodable {
+    let name: String
+    let age: Double
 }
 
-struct Info_Previews: PreviewProvider {
-    static var previews: some View {
-        Info()
-    }
+extension Info {
+    // For testing usage
+    static let dummyData: [Info] = [
+        Info(name: "Paris", age: 34)
+        Info(name: "Michael", age: 69)
+        Info(name: "Julie", age: 27)
+        Info(name: "Catherine", age: 67)
+        Info(name: "Abbigale", age: 25)
+    ]
 }
